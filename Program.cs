77faef88be92d890dc6645794a9e3951a800396b
Vlad Averyanov <averyanovvk@gmail.com>
2021-08-17 22:06:36 +0300

    uploading task

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "123456789qwertyuioplkjhgfdsazxcvbnm";
+
+            Console.WriteLine(Reverse(text));
+
+        }
+
+        public static string Reverse(string s)
+        {
+            if (s == null || s.Length <= 1) 
+                return s;
+            return Reverse(s.Substring(1)) + s[0];
+        }
+    }
+}
